b763bf4a1ecca0875ed940e697fe828870b9fdef
MrAzuka <johntwice8@gmail.com>
2020-11-14 14:40:43 +0100

    New page

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 # Create your views here.
+
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
     context_object_name = 'all_post_list'
 
+
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'article']

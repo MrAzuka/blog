78b4c56bf98553c357fecb155daac956a2292481
MrAzuka <johntwice8@gmail.com>
2020-11-16 14:58:34 +0100

    Added Tests for new features

--- blog/tests.py
+++ blog/tests.py
@@ -42,3 +42,25 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+
+    def test_create_post_view(self):
+        res = self.client.post(reverse('post_new'), {
+            'title': 'New title', 'author': self.user, 'article': 'Test server'
+        })
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, 'Test server', 1)
+        self.assertNotContains(res, 'Not a boy')
+
+    def test_update_post_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title', 'article': 'Updated text',
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_post_view(self):
+        response = self.client.post(
+            reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)

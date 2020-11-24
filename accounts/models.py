82bdb7827453cbc9301960ade53241ab53bf8bea
MrAzuka <johntwice8@gmail.com>
2020-11-24 16:19:06 +0100

    New Custom User Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    STUDENT = 'STUD'
+    EMPLOYED = 'EMPD'
+    UNEMPLOYED = 'UEMP'
+    OTHERS = 'OTHE'
+    CAREER_CHOICES = [
+        (STUDENT, 'STUD'),
+        (EMPLOYED, 'EMPD'),
+        (UNEMPLOYED, 'UEMP'),
+        (OTHERS, 'OTHE')
+    ]
+    career_choices = models.CharField(
+        max_length=4,
+        choices=CAREER_CHOICES,
+        default=STUDENT
+    )
+    age = models.PositiveIntegerField(null=True, blank=True)

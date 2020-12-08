9af400ea2bfbc62068da3b64fc00af0d1dedaed2
MrAzuka <johntwice8@gmail.com>
2020-12-08 13:56:55 +0100

    Minor Changes

--- accounts/models.py
+++ accounts/models.py
@@ -3,20 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
 class CustomUser(AbstractUser):
-    STUDENT = 'STUD'
-    EMPLOYED = 'EMPD'
-    UNEMPLOYED = 'UEMP'
-    OTHERS = 'OTHE'
-    CAREER_CHOICES = [
-        (STUDENT, 'STUD'),
-        (EMPLOYED, 'EMPD'),
-        (UNEMPLOYED, 'UEMP'),
-        (OTHERS, 'OTHE')
-    ]
-    career_choices = models.CharField(
-        max_length=4,
-        choices=CAREER_CHOICES,
-        default=STUDENT
-    )
     age = models.PositiveIntegerField(null=True, blank=True)
